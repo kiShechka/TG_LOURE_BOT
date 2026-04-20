72e6c01627ef3672e498bc6be922330dda7f44b4
ki <ki.osnovnoi@gmail.com>
2026-04-20 22:17:36 +0300

    Update profile_view.py

--- Loure_Bot/handlers/profile_view.py
+++ Loure_Bot/handlers/profile_view.py
@@ -10,7 +10,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.enums import ParseMode
 
 from config import INDUSTRIES, TARGETS, ADMIN_CHAT_ID, DB_PATH
-from database.crud import get_profile_by_user_id, get_recommended_profiles, get_visit_count,increment_daily_visit, increment_visit_count,save_response, check_response, get_responses_count, get_active_chat_by_users,get_user_active_chat,save_message, close_chat, is_user_banned,get_profile_by_code,get_reactions,save_reaction,update_activity,get_user_profiles
+from database.crud import get_profile_by_user_id, get_recommended_profiles, get_visit_count,increment_daily_visit, increment_visit_count,save_response, check_response, get_responses_count, get_active_chat_by_users,get_user_active_chat,save_message, close_chat, is_user_banned,get_profile_by_code,get_reactions,save_reaction,update_activity
 from utils.filters import apply_filters
 
 logger = logging.getLogger(__name__)
@@ -277,7 +277,7 @@ async def view_my_profile(message_or_callback: Message | CallbackQuery):
     await message_or_callback.answer("<b>Ваши анкеты:</b>\n", parse_mode=ParseMode.HTML)
     
     for profile in profiles:
-        await send_simple_profile(message_or_callback, profile)
+        await show_current_profile(message_or_callback, profile)
         
         buttons = []
         if profile.get('is_active'):
