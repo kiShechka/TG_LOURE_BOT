06bd9276b3f0275993b3891b6206a32269da8528
ki <ki.osnovnoi@gmail.com>
2026-05-19 04:11:57 +0300

    Update profile_creanion.py

--- Loure_Bot/handlers/profile_creanion.py
+++ Loure_Bot/handlers/profile_creanion.py
@@ -80,6 +80,9 @@ async def send_profile_to_admins(bot, profile: dict, admin_chat_id: int):
             f"<b>Описание:</b>\n{profile['description']}\n\n"
             f"<b>Ищет:</b> {TARGETS[profile['target']]}"
         )
+         keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="❌ Удалить анкету", callback_data=f"delete_{profile['code']}")]
+        ])
         await bot.send_message(
             chat_id=admin_chat_id,
             text="_________",
